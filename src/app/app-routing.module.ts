d8608685e1bc8efacfd1cc609f959875b4a3a00f
veni75 <vardai.eniko@gmail.com>
2021-07-20 19:49:10 +0200

    route guards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,13 @@ import { HomeComponent } from './page/home/home.component';
 import { LoginComponent } from './page/login/login.component';
 import { UserEditComponent } from './page/user-edit/user-edit.component';
 import { UsersComponent } from './page/users/users.component';
+import { AuthGuardService } from './service/auth-guard.service';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
+    canActivate:[AuthGuardService],
   },
   {
     path: 'login',
@@ -18,10 +20,12 @@ const routes: Routes = [
   {
     path: 'users',
     component: UsersComponent,
+    canActivate:[AuthGuardService],
   },
   {
     path: 'user/edit/:id',
     component: UserEditComponent,
+    canActivate:[AuthGuardService],
   },
   {
     path: 'forbidden',
